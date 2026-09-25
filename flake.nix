19b64aed0c411776538d137b94ff5fb3bce7d18f
FumiHubCNS <fendo@cns.s.u-tokyo.ac.jp>
2026-09-25 16:55:56 +0900

    Replace training config with example config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Artemis working directories manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.writeShellApplication {
+          name = "artlogin";
+
+          text = ''
+            exec ${./bin/artlogin-nix} "$@"
+          '';
+        };
+      });
+    };
+}
